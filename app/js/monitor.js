88bc1cf131fd6d94cbb46d470ce83c641608a6fa
stefaleon <stefaleon@gmail.com>
2020-06-05 03:32:50 +0300

    Notification Timing

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -4,13 +4,8 @@ const cpu = osu.cpu;
 const mem = osu.mem;
 const os = osu.os;
 
-const cpuOverload = 80;
-
-notifyUser({
-  title: 'CPU Overload',
-  body: `CPU is over ${cpuOverload}%`,
-  icon: path.join(__dirname, 'img', 'icon.png'),
-});
+const cpuOverload = 5;
+const alertFrequency = 1;
 
 // Run every 2 seconds
 setInterval(() => {
@@ -25,6 +20,17 @@ setInterval(() => {
       document.getElementById('cpu-progress').style.background = 'red';
     } else {
       document.getElementById('cpu-progress').style.background = '#30c88b';
+    }
+
+    // Check overload
+    if (info >= cpuOverload && runNotify(alertFrequency)) {
+      notifyUser({
+        title: 'CPU Overload',
+        body: `CPU is over ${cpuOverload}%`,
+        icon: path.join(__dirname, 'img', 'icon.png'),
+      });
+
+      localStorage.setItem('lastNotify', +new Date());
     }
   });
 
@@ -64,4 +70,23 @@ function secondsToDhms(seconds) {
 // Send notification
 function notifyUser(options) {
   new Notification(options.title, options);
+}
+
+// Check how much time has passed since notification
+function runNotify(frequency) {
+  if (localStorage.getItem('lastNotify') === null) {
+    // Store timestamp
+    localStorage.setItem('lastNotify', +new Date());
+    return true;
+  }
+  const notifyTime = new Date(parseInt(localStorage.getItem('lastNotify')));
+  const now = new Date();
+  const diffTime = Math.abs(now - notifyTime);
+  const minutesPassed = Math.ceil(diffTime / (1000 * 60));
+
+  if (minutesPassed > frequency) {
+    return true;
+  } else {
+    return false;
+  }
 }
